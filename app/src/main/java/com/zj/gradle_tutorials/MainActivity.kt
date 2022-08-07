193ec20104ebb5e94c45b05800bad5472664f4ab
RicardoJiang <2868405029@qq.com>
2022-08-07 15:18:48 +0800

    ADD: init project

---
+++ app/src/main/java/com/zj/gradle_tutorials/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zj.gradle_tutorials
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
