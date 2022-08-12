33bf827213bb23f4a0818b4fcb84ed7f768b2e2a
RicardoJiang <2868405029@qq.com>
2022-08-12 09:18:55 +0800

    ADD: custom transfromaction

--- transform-action/custom-transform/src/main/java/com/zj/android/transform/action/TransformActionPlugin.kt
+++ transform-action/custom-transform/src/main/java/com/zj/android/transform/action/TransformActionPlugin.kt
@@ -2,9 +2,34 @@ package com.zj.android.transform.action
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.attributes.Attribute
+import java.io.File
 
 class TransformActionPlugin : Plugin<Project> {
     override fun apply(project: Project) {
-        println("使用模板创建的插件")
+        project.run {
+            val artifactType = Attribute.of("artifactType", String::class.java)
+            dependencies.registerTransform(MyTransform::class.java) {
+                it.from.attribute(artifactType, "jar")
+                it.to.attribute(artifactType, "my-custom-type")
+            }
+            val myTaskProvider = tasks.register("myTask", MyTask::class.java) {
+                it.inputCount.set(10)
+                it.outputFile.set(File("build/myTask/output/file.jar"))
+            }
+            val includedConfiguration = configurations.create("includedConfiguration")
+            dependencies.add(includedConfiguration.name, "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.10")
+
+            val combinedInputs = project.files(includedConfiguration, myTaskProvider.map { it.outputFile })
+            val myConfiguration = configurations.create("myConfiguration")
+            dependencies.add(myConfiguration.name, project.files(project.provider { combinedInputs }))
+
+            tasks.register("consumerTask", ConsumerTask::class.java) {
+                it.artifactCollection = myConfiguration.incoming.artifactView {viewConfiguration ->
+                    viewConfiguration.attributes.attribute(artifactType, "my-custom-type")
+                }.artifacts
+                it.outputFile.set(File("build/consumerTask/output/output.txt"))
+            }
+        }
     }
 }
